d1d84bb2d7dbdc0dc1c4e2f174fdb5976a647610
chneau <charles63500@gmail.com>
2021-04-11 18:33:45 +0100

    working API

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from flask import Flask
-from flask import request
 
 import functions
 
 app = Flask(__name__)
 
 
-@app.route('/call/<function_name>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def call_function(function_name: str):
-    function_to_call = getattr(functions, function_name)
-    body = request.json
-    return function_to_call(body)
+@app.route('/even/<number>', methods=['GET'])
+def call_even(number: int):
+    result = {"number": number}
+
+    try:
+        number = int(number)
+    except ValueError:
+        result["error"] = "A number is expected"
+        return result, 500
+
+    is_even = functions.is_even(number)
+    result["isEven"] = is_even
+    return result
 
 
 app.run(host="0.0.0.0")
--- readme.md
+++ readme.md
@@ -2,6 +2,17 @@
 
 API to know if a number is odd or even
 
+## dev
+
+```bash
+# command to "hotreload" the web page when the code changes
+nodemon -e py -x python main.py
+
+# -e for extensions
+# -x for the command
+
+```
 
 ## To do
+
 - everything
