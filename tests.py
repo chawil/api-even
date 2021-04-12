763da6d532cca30a992e76f1c0e8b87c244f807f
William <william.lepommelet@gmail.com>
2021-04-13 00:14:12 +0200

    add unit test for odd func

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import unittest
 from random import randint
 
 from core import is_even
+from core import is_odd
 
 
 class TestEvenCore(unittest.TestCase):
@@ -9,11 +10,17 @@ class TestEvenCore(unittest.TestCase):
     def test_is_1_even(self):
         self.assertFalse(is_even(1))
 
+    def test_is_1_odd(self):
+        self.assertTrue(is_odd(1))
+
     def test_random_small_number(self):
         for _ in range(9):
             random_int = randint(0, 9)
             even = random_int % 2 == 0
             self.assertEqual(is_even(random_int), even)
+            odd = random_int % 2 == 1
+            self.assertEqual(is_odd(random_int),odd)
+
 
 
 if __name__ == '__main__':
