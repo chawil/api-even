3c0e631229980ebdb22afa10dfaa22a739349e5b
William <william.lepommelet@gmail.com>
2021-04-12 23:06:24 +0200

    feat: new API route to is_odd func

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 from flask import Flask
 
 from core import is_even
+from core import is_odd
 
 app = Flask(__name__)
 
 
-@app.route('/even/<number>', methods=['GET'])
-def call_even(number: int):
-    result = {"number": number}
+@app.route('/even/<input>', methods=['GET'])
+def call_even(input: int):
+    result = {"input": input}
 
     try:
-        number = int(number)
+        number = int(input)
     except ValueError:
         result["error"] = "A number is expected"
         return result, 500
 
     even = is_even(number)
     result["isEven"] = even
+    return result
+
+@app.route('/odd/<input>', methods=['GET'])
+def call_odd(input: int):
+    result = {"input":input}
+    try :
+        number = int(input)
+    except ValueError:
+        result["error"] = "A number is expected"
+        return result, 500
+    odd = is_odd(number)
+    result["isOdd"] = odd
     return result
 
 
